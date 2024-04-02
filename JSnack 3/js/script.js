2c93f3a812516ab0a9020d8a5c25793f58a4ebfd
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-04-02 16:21:55 +0200

    feat: Snack3

---
+++ JSnack 3/js/script.js
@@ -0,0 +1,36 @@
+//Il software deve chiedere per 10 volte all'utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+
+let fNum = parseInt(prompt("Dimmi il primo numero")); //number
+console.log(fNum);
+
+let sNum = parseInt(prompt("Dimmi il secondo numero")); //number
+console.log(sNum);
+
+let tNum = parseInt(prompt("Dimmi il terzo numero")); //number
+console.log(tNum);
+
+let foNum = parseInt(prompt("Dimmi il quarto numero")); //number
+console.log(foNum);
+
+let fiNum = parseInt(prompt("Dimmi il quinto numero")); //number
+console.log(fiNum);
+
+let siNum = parseInt(prompt("Dimmi il sesto numero")); //number
+console.log(sNum);
+
+let seNum = parseInt(prompt("Dimmi il settimo numero")); //number
+console.log(seNum);
+
+let eNum = parseInt(prompt("Dimmi il ottavo numero")); //number
+console.log(eNum);
+
+let nNum = parseInt(prompt("Dimmi il nono numero")); //number
+console.log(nNum);
+
+let teNum = parseInt(prompt("Dimmi il decimo numero")); //number
+console.log(teNum);
+
+
+const sum = fNum + sNum + tNum + foNum + fiNum + siNum + seNum + eNum + nNum + teNum;
+console.log(sum);
